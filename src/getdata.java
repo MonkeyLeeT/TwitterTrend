f0c5092e2dc0f7f865f39525f2a9a5fe78b9d3ec
Ted Li <tl2493@columbia.edu>
2015-05-16 09:32:35 +0800

    Lots of changes

---
+++ src/getdata.java
@@ -0,0 +1,62 @@
+
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.io.FileReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class getdata
+ */
+public class getdata extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public getdata() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    	System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
+		BufferedReader reader = new BufferedReader(new FileReader("output.txt"));		
+        Gson gson = new Gson();
+        List<TrendData> trendData = new LinkedList<>();
+        while (true) {
+            String line = reader.readLine();
+            System.out.print(line);
+            if (line == null)
+            	break;
+            TrendData newData = gson.fromJson(line, TrendData.class);
+            trendData.add(newData);
+        }
+        PrintWriter write = response.getWriter();
+        response.setContentType("application/json");
+        write.write(gson.toJson(trendData));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
